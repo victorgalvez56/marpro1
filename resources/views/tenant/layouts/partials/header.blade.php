ceba56b8a889be82ac8fe1ea034beb66e35afce8
vgalvez@itesur.com <nolie123e>
2021-03-24 13:46:22 -0500

    Se agregó menú

--- resources/views/tenant/layouts/partials/header.blade.php
+++ resources/views/tenant/layouts/partials/header.blade.php
@@ -52,160 +52,56 @@
                 <ul id="iq-sidebar-toggle" class="iq-menu d-flex">
                    <li>
                       <a href="#dashboard" class="iq-waves-effect collapsed" data-toggle="collapse" aria-expanded="false"><i class="ri-home-line"></i><span>Dashboard</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
-                      <ul id="dashboard" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
-                         <li><a href="index.html" class="iq-waves-effect"><i class="ri-home-4-line"></i><span>Dashboard 1</span></a></li>
-                         <li><a href="dashboard-1.html" class="iq-waves-effect"><i class="ri-home-3-line"></i><span>Dashboard 2</span></a></li>
-                      </ul>
                    </li>
                    <li class="active">
-                      <a href="#menu-design" class="iq-waves-effect collapsed" data-toggle="collapse" aria-expanded="true"><i class="ri-menu-3-line"></i><span>Menu Design</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
-                      <ul id="menu-design" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
-                         <li class="active"><a href="horizontal-menu.html"><i class="ri-git-commit-line"></i>Horizontal menu</a></li>
-                         <li><a href="horizontal-top-menu.html"><i class="ri-text-spacing"></i>Horizontal Top Menu</a></li>
-                         <li><a href="two-sidebar.html"><i class="ri-indent-decrease"></i>Two Sidebar</a></li>
-                         <li><a href="vertical-top-menu.html"><i class="ri-line-height"></i>Vertical block menu</a></li>
-                      </ul>
+                      <a href="#menu-design" class="iq-waves-effect collapsed" data-toggle="collapse" aria-expanded="true"><i class="ri-menu-3-line"></i><span>Ventas</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
                    </li>
                    <li>
-                      <a href="#menu-level" class="iq-waves-effect collapsed" data-toggle="collapse" aria-expanded="false"><i class="ri-record-circle-line"></i><span>Social</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
+                      <a href="#menu-level" class="iq-waves-effect collapsed" data-toggle="collapse" aria-expanded="false"><i class="ri-record-circle-line"></i><span>Productos</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
                       <ul id="menu-level" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
-                         <li>
-                            <a href="#mailbox" class="iq-waves-effect collapsed"  data-toggle="collapse" aria-expanded="false"><span>Email</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
-                            <ul id="mailbox" class="iq-submenu iq-submenu-data collapse">
-                               <li><a href="app/index.html">Inbox</a></li>
-                               <li><a href="app/email-compose.html">Email Compose</a></li>
-                            </ul>
-                         </li>
-                         <li><a href="chat.html">Chat</a></li>
-                         <li><a href="todo.html">Todo</a></li>
-                         <li><a href="calendar.html">Calendar</a></li>
-                         <li>
-                            <a href="#user-info" class="iq-waves-effect collapsed"  data-toggle="collapse" aria-expanded="false"><span>User</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
-                            <ul id="user-info" class="iq-submenu iq-submenu-data collapse">
-                               <li><a href="profile.html">User Profile</a></li>
-                               <li><a href="profile-edit.html">User Edit</a></li>
-                               <li><a href="add-user.html">User Add</a></li>
-                               <li><a href="user-list.html">User List</a></li>
-                            </ul>
-                         </li>
+                         <li><a href="chat.html">Productos</a></li>
+                         <li><a href="todo.html">Categorías</a></li>
+                         <li><a href="calendar.html">Marcas</a></li>
+                         <li><a href="calendar.html">Series</a></li>
                       </ul>
                    </li>
                    <li>
-                      <a href="#ui-elements" class="iq-waves-effect collapsed"  data-toggle="collapse" aria-expanded="false"><i class="ri-pencil-ruler-line"></i><span>UI Elements</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
-                      <ul id="ui-elements" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle" style="height: 300px; overflow-y: scroll;">
-                         <li><a href="ui-colors.html">colors</a></li>
-                         <li><a href="ui-typography.html">Typography</a></li>
-                         <li><a href="ui-alerts.html">Alerts</a></li>
-                         <li><a href="ui-badges.html">Badges</a></li>
-                         <li><a href="ui-breadcrumb.html">Breadcrumb</a></li>
-                         <li><a href="ui-buttons.html">Buttons</a></li>
-                         <li><a href="ui-cards.html">Cards</a></li>
-                         <li><a href="ui-carousel.html">Carousel</a></li>
-                         <li><a href="ui-embed-video.html">Video</a></li>
-                         <li><a href="ui-grid.html">Grid</a></li>
-                         <li><a href="ui-images.html">Images</a></li>
-                         <li><a href="ui-list-group.html">list Group</a></li>
-                         <li><a href="ui-media-object.html">Media</a></li>
-                         <li><a href="ui-modal.html">Modal</a></li>
-                         <li><a href="ui-notifications.html">Notifications</a></li>
-                         <li><a href="ui-pagination.html">Pagination</a></li>
-                         <li><a href="ui-popovers.html">Popovers</a></li>
-                         <li><a href="ui-progressbars.html">Progressbars</a></li>
-                         <li><a href="ui-tabs.html">Tabs</a></li>
-                         <li><a href="ui-tooltips.html">Tooltips</a></li>
+                      <a href="#ui-elements" class="iq-waves-effect collapsed"  data-toggle="collapse" aria-expanded="false"><i class="ri-file-user-fill"></i><span>Clientes</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
+                      <ul id="ui-elements" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle" >
+                         <li><a href="ui-colors.html">Clientes</a></li>
+                         <li><a href="ui-typography.html">Tipos de clientes</a></li>
                       </ul>
                    </li>
                    
                    <li>
-                      <a href="#Pages" class="iq-waves-effect collapsed" data-toggle="collapse" aria-expanded="false"><i class="ri-record-circle-line"></i><span>Pages</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
-                      <ul id="Pages" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
-                         <li>
-                            <a href="#forms" class="iq-waves-effect collapsed"  data-toggle="collapse" aria-expanded="false"><span>Forms</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
-                            <ul id="forms" class="iq-submenu collapse iq-submenu-data">
-                               <li><a href="form-layout.html">Form Elements</a></li>
-                               <li><a href="form-validation.html">Form Validation</a></li>
-                               <li><a href="form-switch.html">Form Switch</a></li>
-                               <li><a href="form-chechbox.html">Form Checkbox</a></li>
-                               <li><a href="form-radio.html">Form Radio</a></li>
-                            </ul>
-                         </li>
-                         <li>
-                            <a href="#forms-wizard" class="iq-waves-effect collapsed"  data-toggle="collapse" aria-expanded="false"><span>Forms Wizard</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
-                            <ul id="forms-wizard" class="iq-submenu collapse iq-submenu-data">
-                               <li><a href="form-wizard.html">Simple Wizard</a></li>
-                               <li><a href="form-wizard-validate.html">Validate Wizard</a></li>
-                               <li><a href="form-wizard-vertical.html">Vertical Wizard</a></li>
-                            </ul>
-                         </li>
-                          <li>
-                            <a href="#tables" class="iq-waves-effect collapsed"  data-toggle="collapse" aria-expanded="false"><span>Table</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
-                            <ul id="tables" class="iq-submenu collapse iq-submenu-data">
-                               <li><a href="tables-basic.html">Basic Tables</a></li>
-                               <li><a href="data-table.html">Data Table</a></li>
-                               <li><a href="table-editable.html">Editable Table</a></li>
-                            </ul>
-                         </li>
-                         <li>
-                            <a href="#charts" class="iq-waves-effect collapsed"  data-toggle="collapse" aria-expanded="false"><span>Charts</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
-                            <ul id="charts" class="iq-submenu iq-submenu-data collapse">
-                               <li><a href="chart-morris.html">Morris Chart</a></li>
-                               <li><a href="chart-high.html">High Charts</a></li>
-                               <li><a href="chart-am.html">Am Charts</a></li>
-                               <li><a href="chart-apex.html">Apex Chart</a></li>
-                            </ul>
-                         </li>
-                         <li>
-                            <a href="#icons" class="iq-waves-effect collapsed"  data-toggle="collapse" aria-expanded="false"><span>Icons</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
-                            <ul id="icons" class="iq-submenu iq-submenu-data collapse">
-                               <li><a href="icon-dripicons.html">Dripicons</a></li>
-                               <li><a href="icon-fontawesome-5.html">Font Awesome 5</a></li>
-                               <li><a href="icon-lineawesome.html">line Awesome</a></li>
-                               <li><a href="icon-remixicon.html">Remixicon</a></li>
-                               <li><a href="icon-unicons.html">unicons</a></li>
-                            </ul>
-                         </li>
-                         <li>
-                            <a href="#authentication" class="iq-waves-effect collapsed"  data-toggle="collapse" aria-expanded="false"><span>Authentication</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
-                            <ul id="authentication" class="iq-submenu iq-submenu-data collapse">
-                               <li><a href="sign-in.html">Login</a></li>
-                               <li><a href="sign-up.html">Register</a></li>
-                               <li><a href="pages-recoverpw.html">Recover Password</a></li>
-                               <li><a href="pages-confirm-mail.html">Confirm Mail</a></li>
-                               <li><a href="pages-lock-screen.html">Lock Screen</a></li>
-                            </ul>
-                         </li>
-                         <li>
-                            <a href="#map" class="iq-waves-effect collapsed"  data-toggle="collapse" aria-expanded="false"><span>Maps</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
-                            <ul id="map" class="iq-submenu iq-submenu-data collapse">
-                               <li><a href="pages-map.html">Google Map</a></li>
-                               <li><a href="#">Vector Map</a></li>
-                            </ul>
-                         </li>
-                         <li>
-                            <a href="#ecommerce" class="iq-waves-effect collapsed"  data-toggle="collapse" aria-expanded="false"><span>eCommerce</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
-                            <ul id="ecommerce" class="iq-submenu iq-submenu-data collapse" data-parent="#iq-sidebar-toggle">
-                               <li><a href="product.html">Product</a></li>
-                               <li><a href="itemdetails.html">Item Details</a></li>
-                               <li><a href="checkout.html">Checkout</a></li>
-                            </ul>
-                         </li>
-                         <li>
-                            <a href="#extra-pages" class="iq-waves-effect collapsed"  data-toggle="collapse" aria-expanded="false"><span>Extra Pages</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
-                            <ul id="extra-pages" class="iq-submenu iq-submenu-data collapse" data-parent="#iq-sidebar-toggle">
-                               <li><a href="pages-timeline.html">Timeline</a></li>
-                               <li><a href="pages-invoice.html">Invoice</a></li>
-                               <li><a href="blank-page.html">Blank Page</a></li>
-                               <li><a href="pages-error.html">Error 404</a></li>
-                               <li><a href="pages-error-500.html">Error 500</a></li>
-                               <li><a href="pages-pricing.html">Pricing</a></li>
-                               <li><a href="pages-pricing-one.html">Pricing 1</a></li>
-                               <li><a href="pages-maintenance.html">Maintenance</a></li>
-                               <li><a href="pages-comingsoon.html">Coming Soon</a></li>
-                               <li><a href="pages-faq.html">Faq</a></li>
-                            </ul>
-                         </li>
-                      </ul>
-                   </li>
+                     <a href="#ui-elements" class="iq-waves-effect collapsed"  data-toggle="collapse" aria-expanded="false"><i class="ri-shopping-cart-fill"></i><span>Compras</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
+                     <ul id="ui-elements" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle" >
+                        <li><a href="ui-colors.html">Nuevo</a></li>
+                        <li><a href="ui-typography.html">Listado</a></li>
+                        <li><a href="ui-typography.html">Gastos diversos</a></li>
+                        <li><a href="ui-typography.html">Proveedores</a></li>
+
+                     </ul>
+                  </li>
+                  <li>
+                     <a href="#ui-elements" class="iq-waves-effect collapsed"  data-toggle="collapse" aria-expanded="false"><i class="ri-inbox-archive-fill"></i><span>Inventario</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
+                     <ul id="ui-elements" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle" >
+                        <li><a href="ui-colors.html">Movimientos</a></li>
+                        <li><a href="ui-typography.html">Traslados</a></li>
+                        <li><a href="ui-typography.html">Devoluciones</a></li>
+                        <li><a href="ui-typography.html">Reporte Inventario</a></li>
+                     </ul>
+                  </li>
+                  <li>
+                     <a href="#ui-elements" class="iq-waves-effect collapsed"  data-toggle="collapse" aria-expanded="false"><i class="ri-user-3-fill"></i><span>Usuarios/Locales & Series</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
+                     <ul id="ui-elements" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle" >
+                        <li><a href="ui-colors.html">Usuarios</a></li>
+                        <li><a href="ui-typography.html">Establecimientos</a></li>
+                     </ul>
+                  </li>
+                  <li>
+                     <a href="#ui-elements" class="iq-waves-effect collapsed"  data-toggle="collapse" aria-expanded="false"><i class="ri-funds-box-fill"></i><span>Reportes</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
+                  </li>
                 </ul>
              </nav>
              </div>
@@ -219,7 +115,7 @@
                       <div class="hover-circle"><i class="ri-arrow-right-s-line"></i></div>
                    </div>
                 </div>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
                    <ul class="navbar-nav ml-auto navbar-list">
                       <li class="nav-item">
                          <a class="search-toggle iq-waves-effect language-title" href="#"><img src="images/small/flag-01.png" alt="img-flaf" class="img-fluid mr-1" style="height: 16px; width: 16px;" />  <i class="ri-arrow-down-s-line"></i></a>
@@ -373,11 +269,11 @@
                          </div>
                       </li>
                    </ul>
-                </div>
+                </div> -->
                 <ul class="navbar-list">
                    <li>
                       <a href="#" class="search-toggle iq-waves-effect d-flex align-items-center">
-                         <img src="images/user/1.jpg" class="img-fluid rounded mr-3" alt="user">
+                         <img src="{{ asset('vito-bootstrapp/images/user/1.jpg') }}" class="img-fluid rounded mr-3" alt="user">
                          
                       </a>
                       <div class="iq-sub-dropdown iq-user-dropdown">
